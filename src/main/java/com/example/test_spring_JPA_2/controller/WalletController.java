1d02ac6bcb343fe18e96f3556f0c78c22cad367f
irwinekabakti <irwinekabakti@gmail.com>
2024-06-10 21:28:12 +0700

    change:(controller response)

--- src/main/java/com/example/test_spring_JPA_2/controller/WalletController.java
+++ src/main/java/com/example/test_spring_JPA_2/controller/WalletController.java
@@ -19,13 +19,6 @@ public class WalletController {
 
     @PostMapping
     public ResponseEntity<CustomResponse<Wallet>> createWallet(@Valid @RequestBody Wallet wallet) {
-//        try {
-//            Wallet postWallet = walletService.createWallet(wallet.getName(), wallet.getAmount(), wallet.getUserId());
-//            return ResponseEntity.status(HttpStatus.CREATED).body(new CustomResponse<>(HttpStatus.CREATED, "success", "New wallet created successfully", postWallet));
-//        } catch (Exception e) {
-//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new CustomResponse<>(HttpStatus.INTERNAL_SERVER_ERROR, "error", "Failed to create wallet", null));
-//        }
-
         Wallet postWallet = walletService.createWallet(wallet.getName(), wallet.getAmount(), wallet.getUserId());
         if (postWallet != null) {
             CustomResponse<Wallet> response = new CustomResponse<>(HttpStatus.CREATED, "success", "New wallet created successfully", postWallet);
@@ -48,9 +41,11 @@ public class WalletController {
         Wallet wallet = walletService.findById(id);
         if (wallet != null) {
             String walletName = wallet.getName();
-            return ResponseEntity.ok(new CustomResponse<>(HttpStatus.OK, "success", walletName  + " Wallet retrieved successfully", wallet));
+            CustomResponse<Wallet> response = new CustomResponse<>(HttpStatus.OK, "success", walletName + " Wallet retrieved successfully", wallet);
+            return response.toResponseEntity();
         } else {
-            return ResponseEntity.notFound().build();
+            CustomResponse<Wallet> response = new CustomResponse<>(HttpStatus.NOT_FOUND, "error", "Wallet not found", null);
+            return response.toResponseEntity();
         }
     }
 
@@ -58,10 +53,12 @@ public class WalletController {
     public ResponseEntity<CustomResponse<Wallet>> updateWallet(@PathVariable Long id, @Valid @RequestBody Wallet wallet) {
         Wallet updatedWallet = walletService.updateWallet(id, wallet.getName(), wallet.getAmount());
         if (updatedWallet != null) {
-            String walletName = wallet.getName();
-            return ResponseEntity.ok(new CustomResponse<>(HttpStatus.OK, "success", walletName + " Wallet updated successfully", updatedWallet));
+            String walletName = updatedWallet.getName();
+            CustomResponse<Wallet> response = new CustomResponse<>(HttpStatus.OK, "success", walletName + " Wallet updated successfully", updatedWallet);
+            return response.toResponseEntity();
         } else {
-            return ResponseEntity.notFound().build();
+            CustomResponse<Wallet> response = new CustomResponse<>(HttpStatus.NOT_FOUND, "error", "Wallet not found", null);
+            return response.toResponseEntity();
         }
     }
 
@@ -69,9 +66,11 @@ public class WalletController {
     public ResponseEntity<CustomResponse<Wallet>> deleteWallet(@PathVariable Long id) {
         Wallet deletedWallet = walletService.deleteById(id);
         if (deletedWallet != null) {
-            return ResponseEntity.ok(new CustomResponse<>(HttpStatus.OK, "success",  deletedWallet.getName()+ " Wallet deleted successfully", deletedWallet));
+            CustomResponse<Wallet> response = new CustomResponse<>(HttpStatus.OK, "success", deletedWallet.getName() + " Wallet deleted successfully", deletedWallet);
+            return response.toResponseEntity();
         } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new CustomResponse<>(HttpStatus.NOT_FOUND, "error", "Wallet not found", null));
+            CustomResponse<Wallet> response = new CustomResponse<>(HttpStatus.NOT_FOUND, "error", "Wallet not found", null);
+            return response.toResponseEntity();
         }
     }
 }
